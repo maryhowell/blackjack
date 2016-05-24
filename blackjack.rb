22d5818e7d419f1bda20ed8bbbaf18b911860050
Spoonflower <mhowell@spoonflower.com>
2016-05-24 15:54:12 -0400

    6 down 5 to go

--- blackjack.rb
+++ blackjack.rb
@@ -1,7 +1,5 @@
 require "pry"
 
-
-
 class Card
   attr_reader :rank, :suit
   def initialize rank, suit
@@ -18,8 +16,37 @@ class Card
 end
 
 class Deck
-  
+  attr_reader :cards, :drawn_card
+
+  def initialize
+    @cards = (1 .. 52).to_a
+    @drawn_card = []
+  end
+
+  def draw
+    new_card = @cards.shuffle!.pop
+    @drawn_card.push(new_card)
+    new_card
+  end
+
+  def drawn
+    @drawn_card
+  end
 end
 
-# class Hand
-# end
+class Hand
+  attr_reader :value
+
+  def initialize
+    @value = 0
+  end
+
+  def add card_1, card_2 = nil
+    if card_2 == nil
+      @value = @value + card_1.value
+    else
+      @value = card_1.value + card_2.value
+    end
+  end
+
+end
--- blackjack_specs.rb
+++ blackjack_specs.rb
@@ -23,47 +23,47 @@ end
 class TestDeck < Minitest::Test
   def test_counting_cards
     deck = Deck.new
-    assert_equal deck.cards.count, 52
+    assert_equal 52, deck.cards.count
   end
 
-#   def test_counting_draws
-#     deck = Deck.new
-#     deck.draw
-#     assert_equal deck.cards.count, 51
-#   end
-#
-#   def test_tracking_draws
-#     deck = Deck.new
-#     drawn_card = deck.draw
-#     assert_equal deck.cards.count, 51
-#     refute_includes deck.cards, drawn_card
-#     assert_includes deck.drawn, drawn_card
-#   end
+  def test_counting_draws
+    deck = Deck.new
+    deck.draw
+    assert_equal 51, deck.cards.count
+  end
+
+  def test_tracking_draws
+    deck = Deck.new
+    drawn_card = deck.draw
+    assert_equal 51, deck.cards.count
+    refute_includes deck.cards, drawn_card
+    assert_includes deck.drawn, drawn_card
+  end
 end
-#
-# class TestHand < Minitest::Test
-#   def test_hand_value_with_number_cards
-#     hand = Hand.new
-#     hand.add(Card.new(9, :H), Card.new(7, :S))
-#     assert_equal hand.value, 16
-#
-#     hand.add(Card.new(4, :D))
-#     assert_equal hand.value, 20
-#   end
+
+class TestHand < Minitest::Test
+  def test_hand_value_with_number_cards
+    hand = Hand.new
+    hand.add(Card.new(9, :H), Card.new(7, :S))
+    assert_equal 16, hand.value
+
+    hand.add(Card.new(4, :D))
+    assert_equal 20, hand.value
+  end
 #
 #   def test_hand_value_with_face_cards
 #     hand = Hand.new
 #     hand.add(Card.new(9, :H), Card.new(:K, :S))
-#     assert_equal hand.value, 19
+#     assert_equal 19, hand.value
 #   end
 #
 #   def test_hand_value_with_aces
 #     hand = Hand.new
 #     hand.add(Card.new(:A, :H), Card.new(:K, :S))
-#     assert_equal hand.value, 21
+#     assert_equal 21, hand.value
 #
 #     hand.add(Card.new(5, :S))
-#     assert_equal hand.value, 16
+#     assert_equal 16, hand.value
 #   end
 #
 #   def test_busting
@@ -84,4 +84,4 @@ end
 #     hand.add(Card.new(5, :S))
 #     assert_equal hand.to_s, 'AH, KS, 5S'
 #   end
-# end
+end
